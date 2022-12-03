53b3551c171f86ea1e305e14df355d8517b4fd4d
fshabanov <fshabanov01@gmail.com>
2022-12-03 16:49:48 +0400

    DecoratorsHW: - redundant file

--- src/decorators/decorators.js
+++ src/decorators/decorators.js
@@ -1,3 +1,2 @@
 export { initDebounce } from "./init-debounce/init-debounce.decorator.js";
-export { initRouteHandler } from "./init-route-handler/init-route-handler.decorator.js";
 export { initLogger } from "./init-logger/init-logger.decorator.js";
