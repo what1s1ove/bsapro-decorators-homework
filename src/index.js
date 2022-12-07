5d64c85a728f4f08663c8a02b83f7e62139ad264
Oleksii30 <polyanovskyyol@gmail.com>
2022-12-07 22:04:50 +0200

    add decorators to Application methods

--- src/index.js
+++ src/index.js
@@ -18,65 +18,63 @@ const LogLevel = {
   WARNING: "warning",
 };
 
-const initLogger = (cb, logLevel) => {
-  return (req, res) => {
-    const { method, url } = req;
+const logger = (logLevel) => {
+  return (value) => {
+    return (req, res) => {
+      const { method, url } = req;
 
-    console.log(`Log Level: ${logLevel} Method: ${method} URL: ${url}`);
+      console.log(`Log Level: ${logLevel} Method: ${method} URL: ${url}`);
 
-    cb.call(null, req, res);
-  };
+      value.call(null, req, res);
+    };
+  }
+}
+
+const debounce = (delay) => {
+  return (value) => {
+    let lastTimeout = null;
+    return (...args) => {
+      clearInterval(lastTimeout);
+      lastTimeout = setTimeout(() => value.call(null, ...args), delay);
+    };
+  }
 };
 
-const initDebounce = (cb, delay) => {
-  let lastTimeout = null;
+const registerPathHandler = (options) => {
+  return (value) => {
+    const { method, path } = options;
+    app.route({
+      method,
+      url: path,
+      handler: value,
+    });
 
-  return (...args) => {
-    clearInterval(lastTimeout);
-
-    lastTimeout = setTimeout(() => cb.call(null, ...args), delay);
-  };
-};
-
-const initHandler = (cb, options) => {
-  const { method, path } = options;
-
-  app.route({
-    method,
-    url: path,
-    handler: cb,
-  });
-
-  return cb;
-};
+    return value
+  }
+}
 
 class Application {
-  constructor() {
-    this.handleUsersGet = initHandler(
-      initLogger(this.handleUsersGet, LogLevel.LOG),
-      {
-        method: HttpMethod.GET,
-        path: ApiPath.USERS,
-      }
-    );
-    this.handleUserCreate = initHandler(
-      initLogger(this.handleUserCreate, LogLevel.WARNING),
-      {
-        method: HttpMethod.POST,
-        path: ApiPath.USERS,
-      }
-    );
-    this.initDbConnection = initDebounce(this.initDbConnection, 5000);
-  }
 
+  @registerPathHandler({
+    method: HttpMethod.GET,
+    path: ApiPath.USERS,
+    app: this._app
+  })
+  @logger(LogLevel.LOG)
   handleUsersGet(_req, res) {
     return res.send([]);
   }
 
+  @registerPathHandler({
+    method: HttpMethod.POST,
+    path: ApiPath.USERS,
+  })
+  @logger(LogLevel.WARNING)
   handleUserCreate(req, res) {
     return res.send(req.body);
   }
 
+  @debounce(5000)
   initDbConnection() {
     console.log("DB connection was success!");
   }
